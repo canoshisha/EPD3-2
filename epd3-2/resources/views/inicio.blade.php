9a2d4f3e3c014522ac6ef4dd7ee92f1edbc611f9
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-11 20:17:32 +0200

    Realizacion de página de inicio,footer y nav
    He realizado la pagina de inicio, no consigo que se vean las imagenes, he colocado el nav para el uso en todas las vistas, y el footer.

---
+++ epd3-2/resources/views/inicio.blade.php
@@ -0,0 +1,38 @@
+@extends('welcome')
+@section('title',"UPOF1")
+@section('contenido')
+<div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>Bienvenidos a F1 UPO</h1>
+        <p>Encuentra la mejor selección de merchandising de Fórmula 1 en nuestra tienda online.</p>
+        <a href="#" class="btn btn-primary">Comprar ahora</a>
+      </div>
+      <div class="col-md-6">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="/resources/views/img/Merchandising1.jpg" class="d-block w-100" alt="Merchandising 1">
+              </div>
+              <div class="carousel-item">
+                <img src="/resources/views/img/Merchandising2.jpg" class="d-block w-100" alt="Merchandising 2">
+              </div>
+              <div class="carousel-item">
+                <img src="/resources/views/img/Merchandising3.jpg" class="d-block w-100" alt="Merchandising 3">
+              </div>
+
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Siguiente</span>
+            </button>
+          </div>
+
+    </div>
+  </div>
+</div>
+@endsection
