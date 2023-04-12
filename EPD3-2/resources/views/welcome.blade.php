e4583398dc7a1d2b5638db6e077829b9025e92b6
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-12 10:38:25 +0200

    Update welcome.blade.php
    Actualizo el welcome para que coja la imagen de icono

--- EPD3-2/resources/views/welcome.blade.php
+++ EPD3-2/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="/img/icon.png">
+        <link rel="shortcut icon" href="{{URL::asset('/img/icon.png')}}">
         <title>@yield('title')</title>
 
         <!-- Fonts -->
