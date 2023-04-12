67d41917679513eb4f883d788d24c78f3cf0787d
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-12 19:25:00 +0200

    Mejorada
    Vista mejorada de los productos individual

--- epd3-2/resources/views/product_des.blade.php
+++ epd3-2/resources/views/product_des.blade.php
@@ -5,42 +5,61 @@
 @endsection
 @section('contenido')
 <div class="container py-5">
-    <div class="row">
+<div class="card h-100">
+    <div class="row mx-auto">
       <div class="col-md-6">
-        <img src="https://via.placeholder.com/400x400.png?text=Camiseta" class="img-fluid" alt="Camiseta">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://via.placeholder.com/400x400.png?text=Camiseta1" class="d-block w-100" alt="Camiseta1">
+            </div>
+            <div class="carousel-item">
+              <img src="https://via.placeholder.com/400x400.png?text=Camiseta2" class="d-block w-100" alt="Camiseta2">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
       <div class="col-md-6">
         <h1>Camiseta X</h1>
-        <p class="lead">Descipción:<br>
-            Aston Martin Aramco Cognizant F1 2023 Camiseta oficial del piloto del equipo Fernando Alonso
+        <p class="lead">Descripción:<br>
+          Aston Martin Aramco Cognizant F1 2023 Camiseta oficial del piloto del equipo Fernando Alonso
         </p>
         <p class="lead">Precio: 79.99€</p>
         <form action="#">
           <div class="mb-3">
             <label for="talla" class="form-label">Talla</label>
-            <select id="talla" class="form-select">
-              <option selected>Selecciona una talla</option>
-              <option value="S">S</option>
-              <option value="M">M</option>
-              <option value="L">L</option>
-              <option value="XL">XL</option>
-              <option value="XXL">XXL</option>
-              <option value="XXXL">XXXL</option>
+            <select id="talla" class="form-select custom-select">
+              <option class="opt" selected>Selecciona una talla</option>
+              <option class="opt" value="S">S</option>
+              <option class="opt" value="M">M</option>
+              <option class="opt" value="L">L</option>
+              <option class="opt" value="XL">XL</option>
+              <option class="opt" value="XXL">XXL</option>
+              <option class="opt" value="XXXL">XXXL</option>
             </select>
             <br>
             <label for="cantidad" class="form-label">Cantidad</label>
-            <select id="cantidad" class="form-select">
-                <option selected>Selecciona una cantidad</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
+            <select id="cantidad" class="form-select custom-select">
+              <option class="opt" selected>Selecciona una cantidad</option>
+              <option class="opt" value="1">1</option>
+              <option class="opt" value="2">2</option>
+              <option class="opt" value="3">3</option>
+            </select>
           </div>
           <div class="mb-3">
             <button type="submit" class="btn btn-danger">Añadir a la cesta</button>
           </div>
         </form>
       </div>
-    </div>
+    </div></div>
   </div>
+
 @endsection
