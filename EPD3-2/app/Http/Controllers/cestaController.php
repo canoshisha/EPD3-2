f7a77fcfde9be469abd6b69dbce2c34944a7fcfc
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-20 17:12:15 +0200

    Act cesta controller
    Añadimos la cantidades de un producto que esta ya en la cesta, y te muestra un mensaje de producto añadido

--- EPD3-2/app/Http/Controllers/cestaController.php
+++ EPD3-2/app/Http/Controllers/cestaController.php
@@ -60,13 +60,13 @@ class cestaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show()
-    {   
+    {
         // $user = User::where('id', Auth::id())->first();
         $shoppingBasket = ShoppingBasket::where('users_id', Auth::id())->first();
 
-        
+
         // $shopping_basket = DB::table('product_baskets')->where('shopping_basket_id', $shoppingBasket->id )->get();
-        return view('cesta',compact('shoppingBasket'));
+        return view('cesta', compact('shoppingBasket'));
     }
 
     /**
@@ -87,18 +87,33 @@ class cestaController extends Controller
                 'cantidad' => 'required|in:1,2,3'
             ]
         );
-    
         $shopping_basket = ShoppingBasket::where('users_id', Auth::id())->first();
-        $productBasket = new ProductBasket();     
-        $productBasket->size = $request->input('talla');
-        $productBasket->cantidad = $request->input('cantidad');
-        $productBasket->product_id = $request->input('product_id');
-        $productBasket->shopping_basket_id = $shopping_basket->id;
-        $productBasket->save();
-        
+
+        $enCesta = False;
+        foreach ($shopping_basket->productBasket as $productB) {
+            if ($productB->product_id == $request->input('product_id')) {
+                $enCesta = True;
+                $productB->cantidad = $request->input('cantidad') + $productB->cantidad;
+                $productB->save();
+                break;
+            }
+        }
+        if(!$enCesta){
+            $productBasket = new ProductBasket();
+            $productBasket->size = $request->input('talla');
+            $productBasket->cantidad = $request->input('cantidad');
+            $productBasket->product_id = $request->input('product_id');
+            $productBasket->shopping_basket_id = $shopping_basket->id;
+            $productBasket->save();
+        }
 
 
-        return redirect()->route('products.menu');
+
+
+
+
+
+        return redirect()->route('products.menu')->with('mensaje', 'Se ha introducido el producto en la cesta');
 
 
     }
@@ -122,7 +137,7 @@ class cestaController extends Controller
     public function update(Request $request, ShoppingBasket $shoppingBasket)
     {
 
-        
+
 
         $productId = $request->input("productB_id"); // id del producto que deseas eliminar
         $productB = productBasket::find($productId);
@@ -147,9 +162,9 @@ class cestaController extends Controller
 
         foreach ($shoppingBasket->productBasket as $productB) {
 
-            $stock=  $productB->product->stock  - $productB->cantidad;
-            if( $stock < 0 ){
-                return redirect()->route('cesta.show')->withErrors(['mensaje' => 'El producto '.  $productB->product->name . ' no tiene suficiente stock para la venta, retirelo de la cesta']);
+            $stock = $productB->product->stock - $productB->cantidad;
+            if ($stock < 0) {
+                return redirect()->route('cesta.show')->withErrors(['mensaje' => 'El producto ' . $productB->product->name . ' no tiene suficiente stock para la venta, retirelo de la cesta']);
 
             }
         }
@@ -162,16 +177,16 @@ class cestaController extends Controller
         $order->users_id = $shoppingBasket->users_id;
         $order->save();
         foreach ($shoppingBasket->productBasket as $productB) {
-           
+
             $productB->product->stock = $productB->product->stock - $productB->cantidad;
             $productB->product->save();
-            $order->products()->attach($productB->product_id, ['quantity' => $productB->cantidad,'size' => $productB->size]);
+            $order->products()->attach($productB->product_id, ['quantity' => $productB->cantidad, 'size' => $productB->size]);
 
 
         }
-        
 
-        
+
+
 
         $ticket = new Ticket();
         $ticket->price_total = $shoppingBasket->calcularTotal();
@@ -183,7 +198,7 @@ class cestaController extends Controller
 
         $user = User::where('id', Auth::id())->first();
 
-        Mail::to(Auth::user()->email)->send(new OrderConfirmation($user,$order, $ticket));
+        Mail::to(Auth::user()->email)->send(new OrderConfirmation($user, $order, $ticket));
 
         return redirect()->route('inicio')->with('mensaje', 'Pedido realizado con éxito');
 
