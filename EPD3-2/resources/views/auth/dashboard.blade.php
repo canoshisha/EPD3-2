ba39c348571afda0e971455fe06ea5ed70a05f23
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-21 18:54:25 +0200

    Creacion de menu de perfil
    Aqui se podrá  visualizar tu perfil

--- EPD3-2/resources/views/auth/dashboard.blade.php
+++ EPD3-2/resources/views/auth/dashboard.blade.php
@@ -1,12 +1,16 @@
 @extends('auth.template')
-
+@section('title', 'Mi perfil')
+@section('scs')
+<link rel="stylesheet" href="{{ asset('css/perfil.css') }}">
+@endsection
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    Dashboard
+                    <h3>Mi perfil</h3>
                 </div>
 
                 <div class="card-body">
@@ -14,11 +18,64 @@
                     <div class="alert alert-success" role="alert">
                         {{session('status')}}
                     </div>
-                    @endif
+
                     @else
+                    <div class="row">
+                        <div class="col-md-4 mt-4">
+                            <div class="card h-100">
+                                <img class="card-img-top" src="/img/user.png" alt="Editar usuario">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">Editar nombre usuario</h5>
+                                    <p class="card-text">Cambia tu nombre</p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-center">
+                                    <a class="btn btn-danger col-4 mx-auto" href="#">Editar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mt-4">
+                            <div class="card h-100">
+                                <img class="card-img-top" src="/img/orders.png" alt="Mis pedidos">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">Mis pedidos</h5>
+                                    <p class="card-text">Revisa tus pedidos y el estado de los envíos.</p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-center">
+                                    <a class="btn btn-danger col-4 mx-auto" href="{{route('cesta.mispedidos')}}">Ver</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mt-4">
+                            <div class="card h-100">
+                                <img class="card-img-top" src="/img/payment.png" alt="Mi método de pago">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">Mi método de pago</h5>
+                                    <p class="card-text">Administra tus métodos de pago y configura tus preferencias.
+                                    </p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-center">
+                                    <a class="btn btn-danger col-4 mx-auto" href="#">Ver</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mt-4">
+                            <div class="card h-100">
+                                <img class="card-img-top" src="/img/pass.png" alt="Mi método de pago">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">Editar contraseña</h5>
+                                    <p class="card-text">Cambia tu contraseña.
+                                    </p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-center">
+                                    <a class="btn btn-danger col-4 mx-auto" href="#">Editar</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
 
                 </div>
+                @endif
             </div>
         </div>
     </div>
--- epd3-2/resources/views/productos.blade.php
+++ epd3-2/resources/views/productos.blade.php
@@ -5,50 +5,51 @@
 @endsection
 @section('contenido')
 <section class="products">
-  <div class="row">
-    <div class="col-md-12">
-      <h2>Productos</h2>
-      <hr>
-    </div>
     <div class="row">
-      <div class="col">
-        <div class="d-flex justify-content-center">
-          @if (session('mensaje'))
-          <div class="alert alert-success my-4 text-center">
-            {{ session('mensaje') }}
-          </div>
-          @endif
+        <div class="col-md-12">
+            <h2>Productos</h2>
+            <hr>
         </div>
-      </div>
-    </div>
-    @foreach($products as $product)
-    @if($product->stock >0)
-    <div class="product col-md-3">
-      <div class="card h-100 align-items-center justify-content-center">
-        <a href="{{route('producto.descripcion',$product)}}" class="text-decoration-none">
-          <?php
+        <div class="row">
+            <div class="col">
+                <div class="d-flex justify-content-center">
+                    @if (session('mensaje'))
+                    <div class="alert alert-success my-4 text-center">
+                        {{ session('mensaje') }}
+                    </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+        @foreach($products as $product)
+        @if($product->stock >0)
+        <div class="product col-md-3">
+            <div class="card h-100 align-items-center justify-content-center">
+                <a href="{{route('producto.descripcion',$product)}}" class="text-decoration-none">
+                    <?php
           $imagen = $imgProducts->where('products_id', $product->id);
           $img = $imagen->first();
           ?>
-          <div class="d-flex mb-3">
-            <img class="img-fluid img-product align-self-center" src="{{ URL::asset($img->routeImg) }}" alt="{{$product->description}}" style="margin-top: 10px; flex-shrink: 0;">
-          </div>
-          <div class="card-body d-flex flex-column justify-content-end">
-            <h3 class="text-center fs-5">{{$product->name}}</h3>
-          </div>
-          <div class="card-footer text-center">
-            <p>{{$product->price}}€</p>
-          </div>
-        </a>
-      </div>
+                    <div class="d-flex mb-3">
+                        <img class="img-fluid img-product align-self-center" src="{{ URL::asset($img->routeImg) }}"
+                            alt="{{$product->description}}" style="margin-top: 10px; flex-shrink: 0;">
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-end">
+                        <h3 class="text-center fs-5">{{$product->name}}</h3>
+                    </div>
+                    <div class="card-footer text-center">
+                        <p>{{$product->price}}€</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        @endif
+
+
+        @endforeach
+
+
     </div>
-    @endif
-
-
-    @endforeach
-
-
-  </div>
 </section>
 
 
