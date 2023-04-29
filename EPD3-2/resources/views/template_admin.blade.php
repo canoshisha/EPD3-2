0ab76d5e06c9087c0c2058999d80be5e8f53b355
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-29 19:57:29 +0200

    Modificacion menu lateral
    modifico menu lateral

--- EPD3-2/resources/views/template_admin.blade.php
+++ EPD3-2/resources/views/template_admin.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-3">
             <!-- Menú lateral -->
             <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action">Dashboard</a>
+                <a href="{{ route('admin.dashboard') }}" class="list-group-item list-group-item-action">Dashboard</a>
                 <a href="#" class="list-group-item list-group-item-action">Usuarios</a>
                 <a href="#" class="list-group-item list-group-item-action">Productos</a>
                 <a href="#" class="list-group-item list-group-item-action">Pedidos</a>
