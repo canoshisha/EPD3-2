8161b09a0a5a168510229488a85939177cfe1105
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-30 20:34:52 +0200

    Actualizacion

--- EPD3-2/resources/views/auth/dashboard.blade.php
+++ EPD3-2/resources/views/auth/dashboard.blade.php
@@ -3,109 +3,110 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <h3>Mi perfil</h3>
-                </div>
-
-                <div class="card-body">
-                    @if(session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{session('status')}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Mi perfil</h3>
                     </div>
 
-                    @else
-                    <div class="row">
-                        <div class="col">
-                            <div class="d-flex justify-content-center">
-                                @if (session('mensaje'))
-                                <div class="alert alert-success my-4 text-center">
-                                    {{ session('mensaje') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @else
+                            <div class="row">
+                                <div class="col">
+                                    <div class="d-flex justify-content-center">
+                                        @if (session('mensaje'))
+                                            <div class="alert alert-success my-4 text-center">
+                                                {{ session('mensaje') }}
+                                            </div>
+                                        @endif
+                                    </div>
                                 </div>
+                            </div>
+                            <div class="row">
+
+                                <div class="col-md-4 mt-4">
+                                    <div class="card h-100">
+                                        <img class="card-img-top" src="/img/user.png" alt="Editar usuario">
+                                        <div class="card-body d-flex flex-column">
+                                            <h5 class="card-title">Editar nombre usuario</h5>
+                                            <p class="card-text">Cambia tu nombre</p>
+                                        </div>
+                                        <div class="card-footer d-flex justify-content-center">
+                                            <a class="btn btn-danger btn-block" href="#">Editar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 mt-4">
+                                    <div class="card h-100">
+                                        <img class="card-img-top" src="/img/orders.png" alt="Mis pedidos">
+                                        <div class="card-body d-flex flex-column">
+                                            <h5 class="card-title">Mis pedidos</h5>
+                                            <p class="card-text">Revisa tus pedidos y el estado de los envíos.</p>
+                                        </div>
+                                        <div class="card-footer d-flex justify-content-center">
+                                            <a class="btn btn-danger btn-block"
+                                                href="{{ route('cesta.mispedidos') }}">Ver</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 mt-4">
+                                    <div class="card h-100">
+                                        <img class="card-img-top" src="/img/payment.png" alt="Mi método de pago">
+                                        <div class="card-body d-flex flex-column">
+                                            <h5 class="card-title">Mi método de pago</h5>
+                                            <p class="card-text">Administra tus métodos de pago y configura tus
+                                                preferencias.
+                                            </p>
+                                        </div>
+                                        <div class="card-footer d-flex justify-content-center">
+                                            <a class="btn btn-danger btn-block"
+                                                href="{{ route('creditCard.read') }}">Ver</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 mt-4">
+                                    <div class="card h-100">
+                                        <img class="card-img-top" src="/img/pass.png" alt="Mi método de pago">
+                                        <div class="card-body d-flex flex-column">
+                                            <h5 class="card-title">Editar contraseña</h5>
+                                            <p class="card-text">Cambia tu contraseña.
+                                            </p>
+                                        </div>
+                                        <div class="card-footer d-flex justify-content-center">
+                                            <a class="btn btn-danger btn-block" href="#">Editar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                @if (Auth::check() && Auth::user()->is_admin)
+                                    <div class="col-md-4 mt-4">
+                                        <div class="card h-100">
+                                            <img class="card-img-top" src="/img/admin.png" alt="Mi método de pago">
+                                            <div class="card-body d-flex flex-column">
+                                                <h5 class="card-title">Dashboard admin</h5>
+                                                <p class="card-text">Ingresa al panel de administración para gestionar tu
+                                                    sitio web
+                                                </p>
+                                            </div>
+                                            <div class="card-footer d-flex justify-content-center">
+                                                <a class="btn btn-danger btn-block" href="{{ route('admin.dashboard') }}"
+                                                    class="nav-link">Ir</a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 @endif
                             </div>
-                        </div>
+
+
                     </div>
-                    <div class="row">
-
-                        <div class="col-md-4 mt-4">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="/img/user.png" alt="Editar usuario">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">Editar nombre usuario</h5>
-                                    <p class="card-text">Cambia tu nombre</p>
-                                </div>
-                                <div class="card-footer d-flex justify-content-center">
-                                    <a class="btn btn-danger col-4 mx-auto" href="#">Editar</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mt-4">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="/img/orders.png" alt="Mis pedidos">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">Mis pedidos</h5>
-                                    <p class="card-text">Revisa tus pedidos y el estado de los envíos.</p>
-                                </div>
-                                <div class="card-footer d-flex justify-content-center">
-                                    <a class="btn btn-danger col-4 mx-auto" href="{{route('cesta.mispedidos')}}">Ver</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mt-4">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="/img/payment.png" alt="Mi método de pago">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">Mi método de pago</h5>
-                                    <p class="card-text">Administra tus métodos de pago y configura tus preferencias.
-                                    </p>
-                                </div>
-                                <div class="card-footer d-flex justify-content-center">
-                                    <a class="btn btn-danger col-4 mx-auto"
-                                        href="{{ route('creditCard.read') }}">Ver</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mt-4">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="/img/pass.png" alt="Mi método de pago">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">Editar contraseña</h5>
-                                    <p class="card-text">Cambia tu contraseña.
-                                    </p>
-                                </div>
-                                <div class="card-footer d-flex justify-content-center">
-                                    <a class="btn btn-danger col-4 mx-auto" href="#">Editar</a>
-                                </div>
-                            </div>
-                        </div>
-                        @if (Auth::check() && Auth::user()->is_admin)
-                        <div class="col-md-4 mt-4">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="/img/admin.png" alt="Mi método de pago">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">Dashboard admin</h5>
-                                    <p class="card-text">Ingresa al panel de administración para gestionar tu sitio web
-                                    </p>
-                                </div>
-                                <div class="card-footer d-flex justify-content-center">
-                                    <a class="btn btn-danger col-4 mx-auto" href="{{ route('admin.dashboard') }}"
-                                        class="nav-link">Ir</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        @endif
-                    </div>
-
-
+                    @endif
                 </div>
-                @endif
             </div>
         </div>
     </div>
-</div>
 @endsection
--- EPD3-2/resources/views/dashboard.blade.php
+++ EPD3-2/resources/views/dashboard.blade.php
@@ -1,87 +1,87 @@
 @extends('template_admin')
 @section('scs')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" />
 @endsection
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
 @endsection
 @section('contenido')
-<!-- Gráficos y datos -->
-<h1>Dashboard con gráficos y datos</h1>
-<hr>
-<div class="row">
-    <div class="col-lg-12">
-        <!-- Gráfico de barras -->
-        <canvas id="grafico-barras"></canvas>
+    <!-- Gráficos y datos -->
+    <h1>Dashboard con gráficos y datos</h1>
+    <hr>
+    <div class="row">
+        <div class="col-lg-12">
+            <!-- Gráfico de barras -->
+            <canvas id="grafico-barras"></canvas>
+        </div>
+        <div class="col-lg-12">
+            <!-- Gráfico de pastel -->
+            <canvas id="grafico-pastel"></canvas>
+        </div>
     </div>
-    <div class="col-lg-12">
-        <!-- Gráfico de pastel -->
-        <canvas id="grafico-pastel"></canvas>
-    </div>
-</div>
-<script>
-var ctx = document.getElementById('grafico-barras').getContext('2d');
-var graficoBarras = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: <?php echo $nombresProductos; ?>,
-        datasets: [{
-            label: 'Productos más vendidos',
-            data: <?php echo $cantidadesVendidas; ?>,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+    <script>
+        var ctx = document.getElementById('grafico-barras').getContext('2d');
+        var graficoBarras = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: <?php echo $nombresProductos; ?>,
+                datasets: [{
+                    label: 'Productos más vendidos',
+                    data: <?php echo $cantidadesVendidas; ?>,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
                 }
-            }]
-        }
-    }
-});
-var ctx2 = document.getElementById('grafico-pastel').getContext('2d');
-var graficoPastel = new Chart(ctx2, {
-    type: 'pie',
-    data: {
-        labels: <?php echo json_encode($nombresProductos_fav); ?>,
-        datasets: [{
-            label: 'Cantidad de favoritos',
-            data: <?php echo json_encode($cantidadFavoritos); ?>,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false
-    }
-});
-</script>
+            }
+        });
+        var ctx2 = document.getElementById('grafico-pastel').getContext('2d');
+        var graficoPastel = new Chart(ctx2, {
+            type: 'pie',
+            data: {
+                labels: <?php echo json_encode($nombresProductos_fav); ?>,
+                datasets: [{
+                    label: 'Cantidad de favoritos',
+                    data: <?php echo json_encode($cantidadFavoritos); ?>,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    </script>
 @endsection
