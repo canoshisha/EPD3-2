532139ac33c2570de2cc5d6452d308bf70a5dceb
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-04-30 21:17:31 +0200

    Actualizaciones de vistas

--- EPD3-2/resources/views/tarjeta_create.blade.php
+++ EPD3-2/resources/views/tarjeta_create.blade.php
@@ -1,38 +1,50 @@
-@extends('welcome')
-@section('title',"UPOF1")
-@section('scs')
-<link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
-@endsection
-@section('contenido')
-<div class="container">
-    <form method="POST" action="{{route('creditCard.create')}}">
-        @method('get')
-        @csrf
-    
-        <div class="form-group">
-            <label for="titular_tarjeta">Titular de la Tarjeta</label>
-            <input type="text" name="titular_tarjeta" class="form-control" id="titular_tarjeta" placeholder="Nombre y Apellido">
-        </div>
-    
-        <div class="form-group">
-            <label for="numero_tarjeta">Número de Tarjeta</label>
-            <input type="number" name="numero_tarjeta" class="form-control" id="numero_tarjeta" placeholder="xxxxxxxxxxxxxxxx">
-        </div>
-    
-        <div class="form-group">
-            <label for="fecha_caducidad">Fecha de Caducidad</label>
-            <input type="text" name="fecha_caducidad" class="form-control" id="fecha_caducidad" placeholder="mm/aa">
-        </div>
-    
-        <div class="form-group">
-            <label for="cvc">CVC</label>
-            <input type="password" name="cvc" class="form-control" id="cvc" placeholder="xxx">
-        </div>
-    
-        <button type="submit" class="btn btn-primary">Guardar</button>
-        <a href="{{route('creditCard.read')}}" class="btn btn-danger col-4 mx-auto">Volver</a>
-    </form>
-</div>
+@extends('auth.template')
+@section('title', 'Crear tarjetas')
 
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Crear tarjeta</h3>
+                    </div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('creditCard.create') }}">
+                            @method('get')
+                            @csrf
+
+                            <div class="form-group">
+                                <label for="titular_tarjeta">Titular de la Tarjeta</label>
+                                <input type="text" name="titular_tarjeta" class="form-control" id="titular_tarjeta"
+                                    placeholder="Nombre y Apellido">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="numero_tarjeta">Número de Tarjeta</label>
+                                <input type="number" name="numero_tarjeta" class="form-control" id="numero_tarjeta"
+                                    placeholder="xxxxxxxxxxxxxxxx">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="fecha_caducidad">Fecha de Caducidad</label>
+                                <input type="text" name="fecha_caducidad" class="form-control" id="fecha_caducidad"
+                                    placeholder="mm/aa">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cvc">CVC</label>
+                                <input type="password" name="cvc" class="form-control" id="cvc" placeholder="xxx">
+                            </div>
+                            <hr>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <a href="{{ route('creditCard.read') }}" class="btn btn-danger col-4 mx-auto">Volver</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
--- EPD3-2/resources/views/tarjeta_read.blade.php
+++ EPD3-2/resources/views/tarjeta_read.blade.php
@@ -1,32 +1,48 @@
-@extends('welcome')
-@section('title',"UPOF1")
-@section('scs')
-<link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
-@endsection
-@section('contenido')
-    <div class="container">
-        <h1>Información Tarjeta de Crédito</h1>
-        @if ($tarjeta == null)
-            <p>No tiene ninguna tarjeta registrada.</p>
-            <a href="{{url('tarjeta_create')}}" class="btn btn-danger col-4 mx-auto">Crear Tarjeta</a>
-        @else
-        <div class="card" style="width: 18rem;">
-            <img src="{{URL::asset('/img/visa-dual.png')}}" class="card-img-top" alt="imagen tarjeta Visa">
-            <div class="card-body">
-              <h5 class="card-title">Tarjeta de credito #{{$tarjeta->id}}</h5>
-              <ul class="list-group">
-                <li class="list-group-item"><strong>Número de la tarjeta:</strong> {{$tarjeta->numero_tarjeta}}</li>
-                <li class="list-group-item"><strong>Fecha de caducidad: </strong>{{$tarjeta->fecha_caducidad}}</li>
-              </ul>
-              
+@extends('auth.template')
+@section('title', 'Tarjetas')
+
+@section('content')
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Información Tarjeta de Crédito</h1>
+                    </div>
+
+                    <div class="card-body">
+
+                        @if ($tarjeta == null)
+                            <p>No tiene ninguna tarjeta registrada.</p>
+                            <a href="{{ url('tarjeta_create') }}" class="btn btn-danger col-4 mx-auto">Crear Tarjeta</a>
+                        @else
+                            <div class="card" style="width: 18rem;">
+                                <img src="{{ URL::asset('/img/visa-dual.png') }}" class="card-img-top"
+                                    alt="imagen tarjeta Visa">
+                                <div class="card-body">
+                                    <h5 class="card-title">Tarjeta de credito #{{ $tarjeta->id }}</h5>
+                                    <ul class="list-group">
+                                        <li class="list-group-item"><strong>Número de la tarjeta:</strong>
+                                            {{ $tarjeta->numero_tarjeta }}
+                                        </li>
+                                        <li class="list-group-item"><strong>Fecha de caducidad:
+                                            </strong>{{ $tarjeta->fecha_caducidad }}
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+
+                            <a href="{{ route('creditCard.delete', $tarjeta->id) }}" class="btn btn-danger ">Eliminar
+                                Tarjeta</a>
+                            <a class="btn btn-primary" href="{{ route('creditCard.edit', $tarjeta) }}">Editar tarjeta</a>
+                        @endif
+                    </div>
+                </div>
             </div>
-          </div>
-    
-          <a href="{{route('creditCard.delete',$tarjeta->id)}}" class="btn btn-danger ">Eliminar Tarjeta</a>
-          <a class="btn btn-primary" href="{{ route('creditCard.edit',$tarjeta) }}">Editar tarjeta</a>
-        @endif
-        
-        
+        </div>
     </div>
-        
+
+    </div>
+
 @endsection
