1144b456d6bd41b7bed6d343cb64c544ab742dad
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-05-16 12:37:05 +0200

    cambio pa matero

--- EPD3-2/resources/views/productos_edit.blade.php
+++ EPD3-2/resources/views/productos_edit.blade.php
@@ -1,49 +1,52 @@
 @extends('template_admin')
 @section('contenido')
-<div class="container">
-    <form method="POST" action="{{route('product.update',$product)}}">
-        
-        @csrf
-        @method('put')
-        <div class="form-group">
-            <label for="name">Nombre del producto</label>
-            <input type="text" name="name" class="form-control" id="name" value="{{$product->name}}">
-        </div>
-        <div class="form-group">
-            <label for="category">Categorías</label>
-            
+    <div class="container">
+        <form method="POST" action="{{ route('product.update', $product) }}">
+
+            @csrf
+            @method('put')
+            <div class="form-group">
+                <label for="name">Nombre del producto</label>
+                <input type="text" name="name" class="form-control" id="name" value="{{ $product->name }}">
+            </div>
+            <div class="form-group">
+                <label for="category">Categorías</label>
+
                 @foreach ($todasCategorias as $category)
-                @if ($categorias->contains($category))
-                <label for="{{ $category->type }}">{{ $category->type }}</label>
-                <input type="checkbox" value="{{ $category->type }}" id="{{ $category->type }}" name="categories[]" checked>
-                @else
-                <label for="{{ $category->type }}">{{ $category->type }}</label>
-                <input type="checkbox" value="{{ $category->type }}" id="{{ $category->type }}" name="categories[]">
-                @endif
+                    @if ($categorias->contains($category))
+                        <label for="{{ $category->type }}">{{ $category->type }}</label>
+                        <input type="checkbox" value="{{ $category->type }}" id="{{ $category->type }}" name="categories[]"
+                            checked>
+                    @else
+                        <label for="{{ $category->type }}">{{ $category->type }}</label>
+                        <input type="checkbox" value="{{ $category->type }}" id="{{ $category->type }}" name="categories[]">
+                    @endif
                 @endforeach
 
-            
-        </div>
-    
-        <div class="form-group">
-            <label for="price">Precio</label>
-            <input type="number" name="price" class="form-control" id="price" value="{{$product->price}}">
-        </div>
 
-    
-        <div class="form-group">
-            <label for="description">Descripción del producto</label>
-            <input type="text" name="description" class="form-control" id="description" value="{{$product->description}}">
-        </div>
-        <div class="form-group">
-            <label for="discount">Descuento</label>
-            <input type="text" name="discount" class="form-control" id="discount" value="{{$product->discount}}">
-        </div>
-        <br>
-        <button type="submit" class="btn btn-primary">Guardar</button>
-        <a href="{{url('/productos')}}" class="btn btn-danger">Volver</a>
-    </form>
-</div>
+            </div>
+
+            <div class="form-group">
+                <label for="price">Precio</label>
+                <input type="number" name="price" class="form-control" id="price" value="{{ $product->price }}">
+            </div>
 
 
+            <div class="form-group">
+                <label for="description">Descripción del producto</label>
+                <input type="text" name="description" class="form-control" id="description"
+                    value="{{ $product->description }}">
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="discount">Descuento</label>
+                <input type="text" name="discount" class="form-control" id="discount" value="{{ $product->discount }}">
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+            <a href="{{ url('/productos') }}" class="btn btn-danger">Volver</a>
+        </form>
+    </div>
 @endsection
