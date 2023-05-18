67876dc0b52b95c99f71d612b9d731d1db438a29
canoshisha <65036904+canoshisha@users.noreply.github.com>
2023-05-18 17:26:36 +0200

    Revert "Revert "arreglos word""
    This reverts commit 2d98da748a661f3e40a817af8237e30e7253c637.

--- EPD3-2/resources/views/welcome.blade.php
+++ EPD3-2/resources/views/welcome.blade.php
@@ -64,29 +64,36 @@
                 <div class="navbar-nav ms-auto">
 
                     @if (Route::has('login'))
-                        <div class="d-flex">
-                            @auth
-                                <a class="nav-link" id="icono_cesta" href="/cesta"><svg xmlns="http://www.w3.org/2000/svg"
-                                        width="16" height="16" fill="currentColor" class="bi bi-cart">
+                        @auth
+                            <a class="nav-link" id="icono_cesta" href="/cesta">
+                                <span class="d-inline d-lg-none">Mi cesta</span>
+                                <span class="d-none d-lg-inline">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-cart">
                                         <path
                                             d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                                    </svg></a>
-                                <a href="{{ url('/home') }}" class="nav-link ">{{ __('messages.my_profile') }}</a>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('messages.logout') }}</a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </li>
-                            @else
-                                <a href="{{ route('login') }}" class="nav-link ">{{ __('messages.login') }}</a>
+                                    </svg>
+                                </span>
+                            </a>
 
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="nav-link ">{{ __('messages.register') }}</a>
-                                @endif
-                            @endauth
-                        </div>
+
+
+
+                            <a href="{{ url('/home') }}" class="nav-link ">{{ __('messages.my_profile') }}</a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('messages.logout') }}</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        @else
+                            <a href="{{ route('login') }}" class="nav-link ">{{ __('messages.login') }}</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="nav-link ">{{ __('messages.register') }}</a>
+                            @endif
+                        @endauth
                     @endif
                 </div>
             </div>
